85d7f4a5e71b91fd3f7c426ea48e9addc121ea45
marianaprovinciato <marimfp131121@gmail.com>
2024-01-19 16:11:24 -0300

    Create WebConfig

--- src/main/java/com/example/todosimple/configs/WebConfig.java
+++ src/main/java/com/example/todosimple/configs/WebConfig.java
@@ -1,4 +1,15 @@
 package com.example.todosimple.configs;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappigns(CorsRegistry registry){
+        registry.addMapping("/**");
+    }
 }
